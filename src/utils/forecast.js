e60fd742a6d30d2f726aa18070e8de078ed19379
David <David@MacBook-Pro-de-Administrador.local>
2020-01-21 17:54:50 +0100

    Humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out, with '+body.currently.humidity+'% of humidity. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
